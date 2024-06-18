0f8d3a1e1d42d17720ebcc34ab6c95397d8f2287
styut <st548482892@gmail.com>
2024-06-18 12:29:12 +0300

    add user api

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Main } from './Component/Main';
+import AddUser from "./Component/addUser.component"
 
 function App() {
   return (
     <div className="App">
 
       <Main></Main>
+      <AddUser/>
+
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ my-app/src/api/user.api.jsx
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export const addUser = (user) => {
+    console.log(id);
+    return axios.put(`http://localhost:3000/meeting/`,user);
+}
+
+
