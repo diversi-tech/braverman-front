0c8a37f59ba14fca3405b60eb04ac013114bfeb7
styut <st548482892@gmail.com>
2024-06-20 13:25:41 +0300

    update login api

--- my-app/src/api/user.api.tsx
+++ my-app/src/api/user.api.tsx
@@ -6,4 +6,9 @@ export const addUser = (user:User) => {
     // return axios.put(`http://localhost:3000/meeting/`,user);
 }
 
+export const LoginUser =(userEmail:string,userPassword:string)=>{
+    debugger
+    console.log(userEmail,userPassword);
+return axios.post(`https://localhost:7119/api/User/Login?UserEmail=${encodeURIComponent(userEmail)}&UserPassword=${encodeURIComponent(userPassword)}`);
+}
 
