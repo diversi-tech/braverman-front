d5078815257053eb649465b6568631ac8a12c86a
TehilaShaulzon <t8472959@gmail.com>
2024-06-26 14:02:25 +0300

    removing comments and fixing the code

--- my-app/src/api/taskCategory.api.tsx
+++ my-app/src/api/taskCategory.api.tsx
@@ -1,13 +1,9 @@
 import axios from 'axios';
-import { TaskCategory } from '../model/taskCategory.model';
-
-let arr = [{ id: "1", description: "qqq" }, { id: "2", description: "www" }]
 
 export const getTaskCategories = async () => {
     try {
-        // const response = await axios.get('https://localhost:7119/api/TaskCategory/getAll');
-        // return [...response.data];
-        return arr;
+        const response = await axios.get('https://localhost:7119/api/TaskCategory/getAll');
+        return [...response.data];
     } catch (error) {
         console.error('Error fetching task categories:', error);
         return [];
@@ -18,14 +14,12 @@ export const getTaskCategories = async () => {
 
 export const addCategory = async (taskCategory: string) => {
 
-    console.log(taskCategory);
-    arr.push({ id: "3", description: taskCategory })
-    return { id: "1", description: taskCategory }
-    // try {
-    //     const response=await axios.post(`https://localhost:7119/api/TaskCategory/addTaskCategory`,taskCategory);
-    //     return response.data; 
-    //   } catch (error) {
-    //     console.error('Error adding task category:', error);
-    //     throw error;
-    //   }
+
+    try {
+        const response=await axios.post(`https://localhost:7119/api/TaskCategory/addTaskCategory`,taskCategory);
+        return response.data; 
+      } catch (error) {
+        console.error('Error adding task category:', error);
+        throw error;
+      }
 }
