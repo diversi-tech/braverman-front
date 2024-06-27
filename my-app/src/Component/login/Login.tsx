9260b6609628a4628371fc9d31f2b48d2dd9e89b
rutiKind <R583209640@gmail.com>
2024-06-27 23:46:12 +0300

    Server-side leads

--- my-app/src/Component/login/Login.tsx
+++ my-app/src/Component/login/Login.tsx
@@ -1,47 +1,63 @@
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { setUser } from '../../Redux/userAction';
+import { useDispatch, useSelector,  } from 'react-redux';
+import { setUser } from '../../Redux/User/userAction';
 import { LoginUser } from '../../api/user.api';
-import { Outlet, useNavigate } from 'react-router-dom';
-import ShowUsers from '../adminScreen/showUsers/ShowUsers.component';
+import { log } from 'console';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
   const [UserEmail, setUserEmail] = useState('');
   const [UserPassword, setUserPassword] = useState('');
-  const [UserType, setUserType] = useState('');
+  const [UserType, setUserType]=useState('');
+  const [UserId,setUserId]=useState('');
+  const [UserTypeId,setUserTypeId]=useState('');
+  const [UserTypeName,setUserTypeName]=useState('');
+  const [UserFirstName,setUserFirstName]=useState('');
+  const [UserLastName,setUserLastName]=useState('');
+
+
+  const currentUser = useSelector((state:{ user: { currentUser: {UserEmail:string,UserPassword:string,UserId:string,UserTypeId:string,UserTypeName:string ,UserFirstName:string,UserLastName:string} } }) => state.user.currentUser);
+//   const currentUser = useSelector((state: { userReducer: { currentUser: { UserEmail: string, UserPassword: string, UserType: string } } }) => state.userReducer.currentUser);
+   console.log(currentUser);
   
+  const dispatch = useDispatch();
+  const nav = useNavigate()
   const navigate = useNavigate();
 
-  const dispatch = useDispatch();
-
-
-  const handleLogin = () => {
-
-
+  const handleLogin  = async () => {
     if (UserEmail && UserPassword) {
       console.log('Logging in with', { UserEmail, UserPassword });
-      LoginUser(UserEmail, UserPassword).
-        then(x => {
-          if (x.status === 200) {
-            console.log(UserType);
-            alert("success")
-            dispatch(setUser({ UserEmail, UserPassword, UserType }));
-            sessionStorage.setItem("isAuthenticated", "true");
+      const response = await LoginUser(UserEmail, UserPassword);
+      if (response.status === 200) {
+        const x = response;
+        console.log(x); 
+          console.log(x.data);
+          debugger
+          console.log(x.data.lastName);
+          console.log(x.data.id);
+          console.log(x.data.firstName);
+          console.log(x.data.userType.id);
+          console.log(x.data.userType.description);
+           setUserLastName(x.data.lastName)
+           setUserId(x.data.id) 
+          setUserFirstName(x.data.firstName)
+         setUserTypeId(x.data.userType.id)
+         setUserTypeName(x.data.userType.description)
+         console.log(UserFirstName);
+         
+         
 
+        alert("success")
+        dispatch(setUser( UserEmail,UserPassword,x.data.id,x.data.userType.id,x.data.userType.description,x.data.firstName,x.data.lastName  ));
+        sessionStorage.setItem("isAuthenticated", "true");
             navigate("/home");
+        
+        }
+        else
+           alert("מייל וסיסמא לא קיימים ")
 
-            debugger
-
-          }
-          else
-            alert("מייל וסיסמא לא קיימים ")
-
-        }).catch(x => {
-          alert("error")
-          console.log("erorr");
-          console.log(x.response);
-        })
-
+      
+      
     } else {
       alert('נא להכניס מייל וסיסמא');
     }
@@ -72,14 +88,9 @@ const Login = () => {
         <button type="button" onClick={handleLogin}>
           התחברות
         </button>
-      </form>
-
-      <Outlet />
-
+  </form>
     </div>
   );
 };
-
-
 
 export default Login;
