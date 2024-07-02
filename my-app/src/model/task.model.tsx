bb049fe0f6db5f5b1f2e749d99142fc25e8f4ff8
chayamitelman <cy0548543690@gmail.com>
2024-07-02 10:12:53 +0300

    add component task

--- my-app/src/model/task.model.tsx
+++ my-app/src/model/task.model.tsx
@@ -1,8 +1,8 @@
-
+import { TaskStatus } from "../enum/taskStatus.enum";
 export interface Task {
     task_id: string;
     task_name: string;
-    status: String; 
+    status: TaskStatus; 
     assigned_to: string;
     comment: string;
   }
