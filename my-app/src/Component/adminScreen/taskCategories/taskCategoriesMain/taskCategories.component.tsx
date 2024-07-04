67d69ba16589af62b63d940b6f8b7a7fec7020e5
TehilaShaulzon <t8472959@gmail.com>
2024-07-04 22:18:38 +0300

    add style to taskCategory components

---
+++ my-app/src/Component/adminScreen/taskCategories/taskCategoriesMain/taskCategories.component.tsx
@@ -0,0 +1,26 @@
+
+import { useState } from "react";
+import { AddTaskCategory } from "../addTaskCategory/addTaskCategory.component";
+import { ShowTasksCategory } from "../showTaskCategories/showTasksCategory.component";
+import './taskCategories.css';
+
+export const TaskCategories = () => {
+  const [refreshCategories, setRefreshCategories] = useState(false);
+  const [showAddCategory, setShowAddCategory] = useState(false);
+
+  const toggleAddCategory = () => {
+    setShowAddCategory(!showAddCategory);
+  };
+
+  const handleCategoryAdded = () => {
+    setRefreshCategories(!refreshCategories);
+    setShowAddCategory(false); // Close the add category component after adding a category
+  };
+
+  return (
+    <>
+      <ShowTasksCategory refresh={refreshCategories} onAddCategoryClick={toggleAddCategory} />
+      {showAddCategory && <AddTaskCategory onCategoryAdded={handleCategoryAdded} onClose={toggleAddCategory} />}
+    </>
+  );
+};
--- my-app/src/api/taskCategory.api.tsx
+++ my-app/src/api/taskCategory.api.tsx
@@ -11,16 +11,31 @@ export const getTaskCategories = async () => {
     }
 };
 
-
-
 export const addCategory = async (taskCategory: TaskCategory) => {
-
-
     try {
         const response=await axios.post(`https://localhost:7119/api/TaskCategory`,taskCategory);
         return response.data; 
       } catch (error) {
         console.error('Error adding task category:', error);
+        throw error;
+      }
+}
+
+export const updateCategory = async (categoryId:string, taskCategory: TaskCategory) => {
+    try {
+        const response=await axios.put(`https://localhost:7119/api/TaskCategory/${categoryId}`,taskCategory);
+        return response.data; 
+      } catch (error) {
+        console.error('Error update task category:', error);
+        throw error;
+      }
+}
+export const deleteCategory = async (categoryId:string) => {
+    try {
+        const response=await axios.delete(`https://localhost:7119/api/TaskCategory/${categoryId}`);
+        return response.data; 
+      } catch (error) {
+        console.error('Error delete task category:', error);
         throw error;
       }
 }
