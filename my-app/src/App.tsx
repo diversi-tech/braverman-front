c7f7521b3914a23d33a984abb45e4bb2c9613e89
rutiKind <R583209640@gmail.com>
2024-07-10 10:47:36 +0300

    gh-pages

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,32 +1,20 @@
 import React, { useEffect } from 'react';
 import './App.css';
-
-
-import { useSelector } from 'react-redux';
-
 import { Outlet, useNavigate } from 'react-router-dom';
 import Nav from './Component/nav/nav.component';
-import { TaskCategories } from './Component/adminScreen/taskCategories/taskCategoriesMain/taskCategories.component';
 
 const App = () => {
-  const isAuthenticated = useSelector((state: { user: { isAuthenticated: boolean } }) => state.user.isAuthenticated);
-  //const isAuthenticated = sessionStorage.getItem('userId')
   const navigate = useNavigate();
 
   useEffect(() => {
-
-    
     if (!sessionStorage.getItem("userId")) {
-      navigate('/Login');
+      navigate('/login');
     }
   }, [navigate]);
 
- 
-
   return (
     <>
-     <Nav></Nav>
-      {/* <Outlet /> */}
+      <Nav />
       <Outlet />
     </>
   );
