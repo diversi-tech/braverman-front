8a2df963cb779c5458665fa66b6dded0de1268d0
rutiKind <R583209640@gmail.com>
2024-07-11 12:39:35 +0300

    server

--- my-app/src/api/projectStatus.api.tsx
+++ my-app/src/api/projectStatus.api.tsx
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
+axios.defaults.baseURL = process.env.REACT_APP_BRAVERMAN;
+const apiUrl = process.env.REACT_APP_BRAVERMAN
+
 export const getStatusProject = () => {
-    return axios.get(`https://localhost:7119/api/LookUpBase/GetAllLookUpBase?e=0`);
+    return axios.get(`${apiUrl}LookUpBase/GetAllLookUpBase?e=0`);
 
 }
 export const filterByStatus = (statusKey:string) => {
-    return axios.get(`https://localhost:7119/api/Project/GetByStatus/${statusKey}`);
+    return axios.get(`${apiUrl}Project/GetByStatus/${statusKey}`);
 }
