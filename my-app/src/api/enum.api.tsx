75034a68a7f403d93eed8ade77070a14e32b9efd
rutiKind <R583209640@gmail.com>
2024-07-14 12:32:50 +0300

    fix

--- my-app/src/api/enum.api.tsx
+++ my-app/src/api/enum.api.tsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 axios.defaults.baseURL = process.env.REACT_APP_BRAVERMAN;
-const apiUrl = process.env.REACT_APP_BRAVERMAN
+// const apiUrl = process.env.REACT_APP_BRAVERMAN
+const apiUrl = 'https://localhost:7119/api/';
+
 export const getAllEnumFromServer = async (int : Number) => {
     let res = await axios.get(`${apiUrl}LookUpBase/GetAllLookUpBase?e=${int}`)
     if (res) {
