7705fae0682053c6c9dfff764982590797a10551
Chagit Cohen <143138541+chagit1@users.noreply.github.com>
2024-07-24 23:41:55 +0300

    Update Timer.model.tsx

--- my-app/src/model/Timer.model.tsx
+++ my-app/src/model/Timer.model.tsx
@@ -1,12 +1,3 @@
-// export interface Timer {
-//     id: number;
-//     description: string;
-//     startTime: string; // זמן התחלה
-//     endTime?: string; // זמן סיום (אופציונלי אם הטיימר נסגר)
-//     duration?: number; // משך הזמן בדקות
-//     running: boolean; // האם הטיימר פועל כרגע
-//   }
-  
 export interface Timer {
   timerId: string;
   startTime: Date;
