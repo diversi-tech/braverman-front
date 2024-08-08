8b8d1c18b5769ba4e3d996292cc35bbd70cf94b3
chayamitelman <150790393+chayamitelman@users.noreply.github.com>
2024-08-08 10:05:43 +0300

    Update dashboard.component.tsx

--- my-app/src/Component/dashboard/dashboard.component.tsx
+++ my-app/src/Component/dashboard/dashboard.component.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react"
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-// import  { Dashboardee } from "./DashboardUser.component";
-// import DashboardComponent, {  } from "./User.component";
+import  { Dashboardee } from "./DashboardUser.component";
+import DashboardComponent, {  } from "./User.component";
 import AttendanceReport from "../Timer/AttendanceReport.component";
 
 export const Dashboard = () => {
