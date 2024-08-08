7a8a431a869f4c2b81f57571c9da5c935842a46d
Ruti Kind <157101445+rutiKind@users.noreply.github.com>
2024-08-08 21:16:35 +0300

    Update DocumentViewer.tsx

--- my-app/src/Component/userScreen/DocumentViewer.tsx
+++ my-app/src/Component/userScreen/DocumentViewer.tsx
@@ -37,7 +37,7 @@ const data = await response.json();
 // רכיב React
 const DocumentViewer: React.FC= () => {
   const [documents, setDocuments] = useState<{type: string, number: string, documentDate: string, description: string, price: string, downloadLink: string }[]>([]);
-const email=essionStorage.getItem("email")
+const email=sessionStorage.getItem("email")
 const [loading, setLoading] = useState<boolean>(false);
   debugger
   // שימוש ב-useEffect לטעינת המסמכים בעת טעינת הקומפוננטה
