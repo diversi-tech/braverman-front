3737367c3963fb4401f088b90597954cedd78450
chayamitelman <cy0548543690@gmail.com>
2024-08-08 10:26:24 +0300

    add css

--- my-app/src/Component/tasks/chat.tsx
+++ my-app/src/Component/tasks/chat.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Chat } from '../../model/chat.nodel';
 import { Directions } from '@mui/icons-material';
+import './chat.css'
 interface Files {
    
     files: File[];
