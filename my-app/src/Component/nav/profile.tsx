857b9f6c2972ed3cb10db8a113c9587da5f6ad3f
chayamitelman <cy0548543690@gmail.com>
2024-08-14 12:06:59 +0300

    add

--- my-app/src/Component/nav/profile.tsx
+++ my-app/src/Component/nav/profile.tsx
@@ -1,15 +1,22 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { AppBar, Toolbar, IconButton, Menu, MenuItem, Typography } from '@mui/material';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import './profile.css'
+import { useNavigate} from 'react-router-dom';
 import Rtl from '../rtl/rtl';
 // import { useLocation, useNavigate } from 'react-router-dom';
 
+
 // const type = sessionStorage.getItem("userType")
 const ProfileIcon: React.FC = () => {
+    const navigate = useNavigate();
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
     const open = Boolean(anchorEl);
+    useEffect(() => {
+        // if (currentUserType === "customer")
+        //   navigate('/not-found');
+      }, [ navigate]);
 //const currentUser = useSelector((state: { user: { currentUser: { UserEmail: string, UserPassword: string, UserId: string, UserTypeId: string, UserTypeName: string, UserFirstName: string, UserLastName: string } } }) => state.user.currentUser);
     const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
         setAnchorEl(event.currentTarget);
@@ -18,11 +25,13 @@ const ProfileIcon: React.FC = () => {
     const handleClose = () => {
         setAnchorEl(null);
     };
-
     const handleLogout = () => {
         console.log('User logged out');
-        window.location.href = '../../login/Login.tsx';
+        // כאן את יכולה להוסיף כל לוגיקה שקשורה ל-logout, כמו מחיקת session או ניקוי state
+        sessionStorage.clear(); // לדוגמה: ניקוי session storage
+        navigate('/Login'); // ניתוב לדף ה-login
     };
+    
     return (
         <div id='imgprof'>
             <IconButton
@@ -51,7 +60,7 @@ const ProfileIcon: React.FC = () => {
                 <MenuItem onClick={handleClose} sx={{ textAlign: 'right',direction:'rtl',fontFamily:'CustomFont' }}>
                     <Typography variant="h6"style={{ fontFamily:'CustomFont',fontSize:'16px'}} >אימייל: {sessionStorage.getItem("email")} </Typography>
                 </MenuItem>
-                <MenuItem onClick={handleLogout} sx={{ textAlign:'center',direction:'rtl',fontFamily:'CustomFont' }}>
+                <MenuItem onClick={ handleLogout} sx={{ textAlign:'center',direction:'rtl',fontFamily:'CustomFont' }}>
                     <Typography variant="button" style={{ fontFamily:'CustomFont',fontSize:'16px', fontWeight:'700' }}>החלף משתמש</Typography>
                 </MenuItem>
             </Menu></div>);
